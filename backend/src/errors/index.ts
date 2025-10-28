4f9accb84c193d46449e8c05d25f6c3ab1aa0491
Flapjacck <plkelly333@me.com>
2025-10-28 19:36:51 -0400

    Issue 96 | Feature: implemented error handling middleware

---
+++ backend/src/errors/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Central export point for all custom error classes
+ * Makes importing errors cleaner throughout the application
+ */
+export { CustomError } from './CustomError';
+export { AuthenticationError } from './AuthenticationError';
+export { ValidationError } from './ValidationError';
+export { NotFoundError } from './NotFoundError';
+export { ServerError } from './ServerError';
