f243edcdf37b984f55566145f69d156fd37b14f8
Flapjacck <plkelly333@me.com>
2026-02-09 14:56:50 -0500

    Fix: moved admin dashboard | removed twmerge util & updated related files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Navbar from '@/components/ui/Navbar'
 import ProtectedRoute from '@/components/ProtectedRoute'
 import StatusDashboard from '@/components/StatusDashboard'
-import Home from '@/components/Home'
 import Welcome from '@/components/Welcome'
 import {
 	SignupForm,
@@ -12,14 +11,13 @@ import {
 	NewPassword,
 	ForgotPassword,
 } from '@/features/auth/components'
-import AdminDashboard2 from '@/components/dashboards/AdminDashboard2'
+import AdminDashboard2 from '@/features/admin/AdminDashboard'
 import JoinATeam from '@/components/JoinATeam'
 import CreateOrJoinTeam from '@/components/CreateOrJoinTeam'
 import InterviewerAvailability from '@/components/InterviewerAvailability'
 import { AuthProvider } from '@/provider/AuthProvider'
-import AdminDashboard from '@/components/dashboards/AdminDashboard'
 import InterviewerDashboard from '@/components/dashboards/InterviewerDashboard'
-import CandidateDashboard from '@/components/dashboards/CandidateDashboard'
+import CandidateDashboard from '@/features/candidate/CandidateDashboard'
 import AdminSettings from './components/AdminSettings'
 
 function App() {
@@ -29,7 +27,6 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<Welcome />} />
-					<Route path="/home" element={<Home />} />
 					<Route path="/status" element={<StatusDashboard />} />
 					<Route path="/signup" element={<SignupForm />} />
 					<Route path="/signin" element={<SigninForm />} />
@@ -43,8 +40,7 @@ function App() {
 					<Route path="/admin-settings" element={<AdminSettings />} />
 					
 					{/* Role-Based Dashboards - Protected */}
-					<Route path="/admin/dashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-					<Route path="/admin/dashboard2" element={<AdminDashboard2 />} />
+					<Route path="/admin/dashboard" element={<ProtectedRoute><AdminDashboard2 /></ProtectedRoute>} />
 					<Route path="/interviewer/dashboard" element={<ProtectedRoute><InterviewerDashboard /></ProtectedRoute>} />
 					<Route path="/candidate/dashboard" element={<ProtectedRoute><CandidateDashboard /></ProtectedRoute>} />
 					
